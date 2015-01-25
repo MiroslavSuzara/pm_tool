414eb2ae5887e9db969689aff9d03ec6dfc9633d
Miroslav Suzara <miroslav.suzara@gmail.com>
2015-01-24 19:02:07 -0800

    Updated the Project model for validation to be unique and in presence of description.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ActiveRecord::Base
+
+  #validates presence of title
+  validates :title, presence: true, uniqueness: true
+
+  validates_presence_of :description, message: "must be present"
+
 end
