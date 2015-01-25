64bf5825d582dde38601abef7a3ab9f65f061b6a
Miroslav Suzara <miroslav.suzara@gmail.com>
2015-01-25 00:49:24 -0800

    Implemented the following:   - form page to create new projects (title, description, due_date)   - show all project view on projects_path   - redirect to projects_path after completing form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,4 +8,24 @@ class ProjectsController < ApplicationController
   @project = Project.find params[:id]
   end
 
+  def new
+  @project = Project.new
+  end
+
+  def create
+    @project = Project.new project_params
+    if @project.save
+      flash[:notice] = "Your Project has been created!"
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :due_date)
+  end
+
 end
